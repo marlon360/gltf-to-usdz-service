d6905c0afd18aaa1c11db554894721bdf2cfda6e
Marlon Lückert <m.lueckert@me.com>
2020-10-25 18:38:47 +0100

    renamed server startup log

--- server/index.js
+++ server/index.js
@@ -30,5 +30,5 @@ app.post('/local-convert', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`USDZ Converter Server running at http://localhost:${port}`)
 })
