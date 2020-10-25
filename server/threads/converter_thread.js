110856493df7fe72c85b06575c911b4a2ebd76c1
Marlon Lückert <m.lueckert@me.com>
2020-10-25 19:42:10 +0100

    build usdz in random tmp dir to prevent conflicts

--- server/threads/converter_thread.js
+++ server/threads/converter_thread.js
@@ -1,5 +1,6 @@
 const { exec } = require("child_process");
-const { workerData, parentPort } = require("worker_threads")
+const { workerData, parentPort } = require("worker_threads");
+const path = require('path');
 
 convertFile(workerData.filename).then((filename) => {
     parentPort.postMessage({ success: true, filename: filename })
@@ -7,10 +8,17 @@ convertFile(workerData.filename).then((filename) => {
     parentPort.postMessage({ success: false, error: error })
 });
 
-function convertFile(filename) {
+function convertFile(filepath) {
+    if (path.isAbsolute(filepath)) {
+        reject("Absolute paths are not allowed!");
+        return;
+    }
+    const filename = path.basename(filepath);
     const nameWithoutExtension = filename.split('.').slice(0, -1).join('.');
+    const dirname = path.dirname(filepath);
+    const tmpFolder = randomString();
     return new Promise((resolve, reject) => {
-        exec(`usd_from_gltf /usr/app/${filename} /usr/app/${nameWithoutExtension}.usdz`, (error, stdout, stderr) => {
+        exec(`usd_from_gltf /usr/app/${filepath} /usr/app/${tmpFolder}/${nameWithoutExtension}.usdz && mv /usr/app/${tmpFolder}/${nameWithoutExtension}.usdz /usr/app/${dirname} && rmdir /usr/app/${tmpFolder}`, (error, stdout, stderr) => {
         if (error) {
             reject(error.message);
             return;
@@ -22,4 +30,8 @@ function convertFile(filename) {
         resolve(`${nameWithoutExtension}.usdz`);
         });
     })
+}
+
+function randomString() {
+    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
 }
